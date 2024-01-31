7ce0671a3d419f7fbba50e3e1b0ad8742160c310
Amit Roy <amitroy497@yahoo.com>
2024-01-31 19:25:22 +0530

    4. Styling component using styles property

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  //   template: '<h3>eKart</h3>',
-  templateUrl: './header.component.html',
   standalone: true,
+  templateUrl: './header.component.html',
+  styles: [
+    'a{text-decoration: none; margin: 0 10px;}',
+    'button{padding: 10px 20px;}',
+    '.ekart--header{width: 100%; height: 70px}',
+  ],
 })
 export class HeaderComponent {}
