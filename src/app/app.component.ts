8cba7e7fe5c5be50d7ad618c71c644c45ec3a814
Amit Roy <amitroy497@yahoo.com>
2024-02-01 14:02:17 +0530

    8. Create component using Angular Cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, TopHeaderComponent],
 })
 export class AppComponent {
   title = 'full-angular-study';
